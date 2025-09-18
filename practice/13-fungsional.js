c9aa63fd4cfebb799cc7178bf95b08ede10b935c
arsenicbreaker <inyourminddx@gmail.com>
2025-09-18 10:45:36 +0700

    initial commit

---
+++ practice/13-fungsional.js
@@ -0,0 +1,11 @@
+
+function getDistance(mph , h){
+    return mph * h }
+//memisahkan data num1 dan num 2 dan fungsi lalu 
+var mph = 60
+var h = 2
+//mengoper data / memakai data tsb dalam fungsi jika hanya ingin
+var distance = getDistance(mph , h)
+//fungsi tsb tidak mengubah variabel global
+
+console.log(distance) //120
