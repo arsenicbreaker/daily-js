3974f23be4ac3d8ecd993821635d81be4d51f6f8
arsenicbreaker <inyourminddx@gmail.com>
2025-09-20 10:19:29 +0700

    initial commit

--- practice/13-fungsional.js
+++ practice/13-fungsional.js
@@ -1,4 +1,4 @@
-
+//contoh 1
 function getDistance(mph , h){
     return mph * h }
 //memisahkan data num1 dan num 2 dan fungsi lalu 
@@ -7,5 +7,16 @@ var h = 2
 //mengoper data / memakai data tsb dalam fungsi jika hanya ingin
 var distance = getDistance(mph , h)
 //fungsi tsb tidak mengubah variabel global
+console.log(distance) // 120
 
-console.log(distance) //120
+
+//contoh 2
+var shoes = 100
+var stateTax = 1.2
+
+function totalPrice(shoes , stateTax) {
+    return shoes * stateTax
+}
+
+var toPay = totalPrice(shoes , stateTax)
+console.log(toPay) // 120
--- practice/14-basicsOOP.js
+++ practice/14-basicsOOP.js
@@ -1,10 +1,23 @@
+//contoh 1
 //object virtualpet mempunyai data berupa properti sleepy dan fungsi nap
 var virtualPet = {
-    sleepy : true, //setiap properti dipisahkan dengan koma
-    nap : function() {
-        this.sleepy = false //this mengacu pada objek virtualPet
-    }
-}
-console.log(virtualPet.sleepy) //true
-virtualPet.nap() //memanggil fungsi nap
-console.log(virtualPet.sleepy) //false
+  sleepy: true, //setiap properti dipisahkan dengan koma
+  nap: function () {
+    this.sleepy = false; //this mengacu pada objek virtualPet
+  },
+};
+console.log(virtualPet.sleepy); //true
+virtualPet.nap(); //memanggil fungsi nap
+console.log(virtualPet.sleepy); //false
+
+//contoh 2
+var purchase1 = {
+  shoes: 100,
+  stateTax: 1.2,
+  totalPrice: function () {
+    var calculation = this.shoes * this.stateTax;
+    console.log("Total :", calculation);
+  },
+};
+
+purchase1.totalPrice();
