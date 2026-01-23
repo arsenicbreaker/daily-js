2ab8f113c98f4103d750112b0560fb9b51aa299e
Agustia Eriani <agustiaeriani@gmail.com>
2026-01-23 16:09:07 +0700

    initial commit

--- state-craft/src/App.jsx
+++ state-craft/src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
   return (
     <div>
+      <header>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </nav>
+        </header>
+    <main>
       <h1>Hello World from ocean!</h1>
       <p>Ini adalah aplikasi React pertama saya 🚀</p>
-    </div>
+    </main>
+  </div>
   );
 }
 
---
+++ state-craft/src/component/card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+  return (
+    <div className="card">
+        <h2>Title</h2>
+        <p>Content</p>
+    </div>
+  )
+}
+export default Card
